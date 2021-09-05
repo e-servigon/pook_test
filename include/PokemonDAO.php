57dee0e64b467fbe7eb18fd42ad25f5e3f5c9e96
eservigon <eservigon@tiendapago.com>
2021-09-05 18:08:19 -0500

    subida inicial de archivos

---
+++ include/PokemonDAO.php
@@ -0,0 +1,49 @@
+<?php
+require_once("PokemonVO.php");
+
+class PokemonDAO{
+    private $host = "localhost";
+    private $user = "sepherot_DemoAdmin";
+    private $password = "Pa73UiXbWG";
+    private $database = "sepherot_DemoDB";
+    private $tabla = "T_Pokemon";
+    private $cnx;
+
+    function __construct(){
+        $this->cnx = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+    }
+
+    function selectAll(){
+        $query = "SELECT * FROM ".$this->tabla;
+        $res = mysqli_query($this->cnx, $query);
+        while($fila=mysqli_fetch_assoc($res)){
+            $vo = new PokemonVO();
+            $vo->setId($fila["ID_Pokemon"]);
+            $vo->setNombre($fila["Pok_Nombre"]);
+            $listadoVo[] = $vo;
+        }
+        return $listadoVo;
+    }
+
+    function insertar($vo){
+        $query = "INSERT INTO ".$this->tabla." (Pok_Nombre) VALUES('".$vo->getNombre()."') ";
+        $res = mysqli_query($this->cnx, $query);
+        if($res){
+            return True;
+        }
+        else{
+            return False;
+        }
+    }
+
+
+}
+
+
+// $dao = new PokemonDAO();
+// $vo = new PokemonVO();
+// $vo->setNombre("Pikachu");
+// $dao->insertar($vo);
+//  print_r($dao->selectAll());
+?>
+
---
+++ include/PokemonVO.php
@@ -0,0 +1,37 @@
+<?php
+ini_set('display_errors', 1);
+
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
+
+class PokemonVO{
+    private $Id;
+    private $Nombre;
+
+    public function setId($valor){
+        $this->Id=$valor;
+    }
+
+    public function setNombre($valor){
+        $this->Nombre=$valor;
+    }
+
+    public function getId(){
+        return $this->Id;
+    }
+
+    public function getNombre(){
+        return $this->Nombre;
+    }
+}
+
+//$vo = new PokemonVO();
+
+//$vo->setId(1);
+
+//echo $vo->getId();
+
+//echo $vo->Id;
+
+?>
