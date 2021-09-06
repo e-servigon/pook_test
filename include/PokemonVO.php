552cade99c30ee706298531a24d4b25ba108521a
eservigon <eservigon@tiendapago.com>
2021-09-05 20:26:05 -0500

    van mas comments

--- include/PokemonVO.php
+++ include/PokemonVO.php
@@ -36,8 +36,10 @@ class PokemonVO{
 //$vo->setId(1);
 //aqui va un comment
 //echo $vo->getId();
+//aqui va otra linea nueva
 //aqui va otro comentario que vamos a probar
 //AQUI VA LA PRUEBA DEL NIVEL
+//aqui va una lina nueva
 
 //echo $vo->Id;
 //echo $vo->Id2;
